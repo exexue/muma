822b72f9012b26429f74ff994696533e3bc3338e
exexue <exexue@gmail.com>
2016-02-27 19:57:06 +0800

    commit

--- fanyi.py
+++ fanyi.py
@@ -14,45 +14,20 @@ running =True
 while running:
 
     url = 'http://fanyi.youdao.com/translate?smartresult=dict&smartresult=rule&smartresult=ugc&sessionFrom=null'
-
     neirong = input('请输入要翻译的内容：')
-
     date = {}
-
     date['type']= 'AUTO'
-
     date['i'] = neirong
-
     date['doctype'] = 'json'
-
     date['xmlVersion'] = '1.6'
-
     date['keyfrom'] = 'fanyi.web'
-
     date['ue'] = 'UTF-8'
-
     date['typoResult'] = 'true'
-
     date = urllib.parse.urlencode(date).encode('utf-8') 
 
-
-
     req = urllib.request.urlopen(url,date)
-
-
-
     html = req.read().decode('utf-8')
-
-
-
     res = r'tgt\":(.*?)\}'
-
-
-
     x = re.findall (res,html)
-
-
-
     for i in x:
-
         print ('翻译结果为：',i)
